2727ae2d6be3f12cc2cfdfd8d1ce8a4106efebab
Tianz <ssstz@outlook.com>
2018-07-17 15:04:20 +0800

    Update layers.py

--- utils/layers.py
+++ utils/layers.py
@@ -10,18 +10,36 @@ def quantize(x):
     return x, s
 
 
-def batch_norm(x, mean, variance, beta, gamma):
-    # x = tf.layers.batch_normalization(x, training=False)
-    # return x
-    return tf.nn.batch_normalization(x, mean, variance, beta, gamma, variance_epsilon=1e-3)
+def batch_norm(x, mean, variance, offset=None, scale=None):
+    return tf.nn.batch_normalization(x, mean, variance, offset, scale, variance_epsilon=1e-3)
 
 
 def conv_2d(x, w, b=None, strides=1, padding='SAME', activation=''):
+    '''
+    2D convolution with quantization (float32-->int8)
+    '''
+    x, sx = quantize(x)
+    w, sw = quantize(w)
+    # Actually, convolution using float32, because of tensorflow does not support int8 conv op
+    x = tf.cast(x, dtype=tf.float32)
+    w = tf.cast(w, dtype=tf.float32)
+    x = tf.nn.conv2d(x, w, strides=[1, strides, strides, 1], padding=padding)
+    # multiply scales
+    x = tf.multiply(x, tf.multiply(sx, sw))
+    if b is not None:
+        x = tf.nn.bias_add(x, b)
+    if activation == 'relu':
+        x = tf.nn.relu(x)
+    return x
+
+
+def depthwise_conv2d(x, w, b=None, strides=1, padding='SAME', activation=''):
     x, sx = quantize(x)
     w, sw = quantize(w)
     x = tf.cast(x, dtype=tf.float32)
     w = tf.cast(w, dtype=tf.float32)
-    x = tf.nn.conv2d(x, w, strides=[1, strides, strides, 1], padding=padding)
+    x = tf.nn.depthwise_conv2d(x, w, strides=[1, strides, strides, 1], padding=padding)
+    # multiply scales
     x = tf.multiply(x, tf.multiply(sx, sw))
     if b is not None:
         x = tf.nn.bias_add(x, b)
