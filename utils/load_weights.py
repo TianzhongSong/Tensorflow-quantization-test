7a85dbabc576b95fbe97db8ea24068ddd4d77812
Tianz <ssstz@outlook.com>
2018-07-18 09:08:24 +0800

    Update load_weights.py

--- utils/load_weights.py
+++ utils/load_weights.py
@@ -4,15 +4,6 @@ import numpy as np
 
 sys.path.append('../')
 
-def quantize(weights):
-    abs_weights = np.abs(weights)
-    vmax = np.max(abs_weights)
-    s = vmax / 127.
-    qweights = weights / s
-    qweights = np.round(qweights)
-    qweights = qweights.astype(np.float32)
-    return qweights, s
-
 
 def weight_loader(weight_file, by_name=False):
     assert by_name, 'argument by_name must be true!'
@@ -26,13 +17,8 @@ def weight_loader(weight_file, by_name=False):
         raise ValueError("weights file must contain attribution: 'layer_names'")
     for layer_name in layers:
         g = f[layer_name]
-        count = 0
         for weight_name in g.attrs['weight_names']:
             weight_value = g[weight_name].value
             name = str(weight_name).split("'")[1]
-            if count == 0:
-                weight_value, s = quantize(weight_value)
-                scale[name] = s
-                count += 1
             weights[name] = weight_value
     return weights, scale
