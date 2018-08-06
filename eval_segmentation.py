d4acc88696196cd6dec252a9e2307db4c97f9cee
Tianz <ssstz@outlook.com>
2018-08-06 21:39:38 +0800

    Update eval_segmentation.py

--- eval_segmentation.py
+++ eval_segmentation.py
@@ -53,13 +53,13 @@ if __name__ == '__main__':
             pbdr.update(1)
             pr = sess.run(prediction, feed_dict={X: x})
             pr = pr.reshape((input_height, input_width, n_classes)).argmax(axis=2)
-            y = y[:, :, 1]
             pt = pr.reshape((input_height * input_width))
-            gt = y.reshape((input_height * input_width))
             for c in range(1, n_classes):
-                gt_img = np.zeros_like(y)
-                pt_img = np.zeros_like(y)
-                gt_img[:] += (gt[:] == c).astype('uint8')
+                gt = y[:, :, c]
+                gt = gt.reshape((input_height * input_width))
+                gt_img = np.zeros_like(gt)
+                pt_img = np.zeros_like(gt)
+                gt_img[:] += (gt[:] == 1).astype('uint8')
                 pt_img[:] += (gt[:] == c).astype('uint8')
                 if not (pt_img == np.zeros_like(pt_img)).all() or not (gt_img == np.zeros_like(gt_img)).all():
                     iou[c - 1] += compute_iou(pt_img[0], gt_img[0])
