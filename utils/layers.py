3825f7a8eabbfa2adbd6217abe01c65b71f5c828
Tianz <ssstz@outlook.com>
2018-08-07 13:26:42 +0800

    Update layers.py

--- utils/layers.py
+++ utils/layers.py
@@ -14,19 +14,19 @@ def batch_norm(x, mean, variance, offset=None, scale=None):
     return tf.nn.batch_normalization(x, mean, variance, offset, scale, variance_epsilon=1e-3)
 
 
-def conv_2d(x, w, b=None, strides=1, padding='SAME', dilations=[1,1,1,1], activation=''):
+def conv_2d(x, w, b=None, weight_scale=0., strides=1, padding='SAME', dilations=[1,1,1,1], activation=''):
     '''
     2D convolution with quantization (float32-->int8)
     '''
+    # quantize input tensor
     x, sx = quantize(x)
-    w, sw = quantize(w)
     # Actually, convolution compute using float32,
     # because of tensorflow has not supported int8 conv op.
     x = tf.cast(x, dtype=tf.float32)
-    w = tf.cast(w, dtype=tf.float32)
     x = tf.nn.conv2d(x, w, strides=[1, strides, strides, 1], padding=padding, dilations=dilations)
     # multiply scales
-    x = tf.multiply(x, tf.multiply(sx, sw))
+    s = sx * weight_scale
+    x = x * s
     if b is not None:
         x = tf.nn.bias_add(x, b)
     if activation == 'relu':
@@ -35,12 +35,7 @@ def conv_2d(x, w, b=None, strides=1, padding='SAME', dilations=[1,1,1,1], activa
 
 
 def depthwise_conv2d(x, w, b=None, strides=1, padding='SAME', activation=''):
-    x, sx = quantize(x)
-    w, sw = quantize(w)
-    x = tf.cast(x, dtype=tf.float32)
-    w = tf.cast(w, dtype=tf.float32)
     x = tf.nn.depthwise_conv2d(x, w, strides=[1, strides, strides, 1], padding=padding)
-    x = tf.multiply(x, tf.multiply(sx, sw))
     if b is not None:
         x = tf.nn.bias_add(x, b)
     if activation == 'relu':
@@ -48,28 +43,23 @@ def depthwise_conv2d(x, w, b=None, strides=1, padding='SAME', activation=''):
     return x
 
 
-def separable_conv2d(x, dw, pw, strides=1, padding='SAME', activation=''):
+def separable_conv2d(x, dw, pw, dw_scale=0., pw_scale=0., strides=1, padding='SAME', activation=''):
     x, sx = quantize(x)
-    dw, sd = quantize(dw)
-    pw, sp = quantize(pw)
     x = tf.cast(x, dtype=tf.float32)
-    dw = tf.cast(dw, dtype=tf.float32)
-    pw = tf.cast(pw, dtype=tf.float32)
     x = tf.nn.separable_conv2d(x, dw, pw, strides=[1, strides, strides, 1], padding=padding)
     # multiply scales
-    x = tf.multiply(x, tf.multiply(sx, tf.multiply(sd, sp)))
+    x = x * sx * dw_scale * pw_scale
     if activation == 'relu':
         x = tf.nn.relu(x)
     return x
 
 
-def denselayer(x, w, b, activation=''):
+def denselayer(x, w, b, weight_scale=0., activation=''):
     x, sx = quantize(x)
-    w, sw = quantize(w)
     x = tf.cast(x, dtype=tf.float32)
-    w = tf.cast(w, dtype=tf.float32)
     x = tf.matmul(x, w)
-    x = tf.multiply(x, tf.multiply(sx, sw))
+    s = sx * weight_scale
+    x = x * s
     x = tf.add(x, b)
     if activation == "relu":
         x = tf.nn.relu(x)
