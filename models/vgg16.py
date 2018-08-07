231a406bb3c5fd2d6dd3fdaf3d3557d7753368ea
Tianz <ssstz@outlook.com>
2018-08-07 13:24:32 +0800

    Update vgg16.py

--- models/vgg16.py
+++ models/vgg16.py
@@ -1,84 +1,95 @@
 from utils.layers import *
 import tensorflow as tf
+import numpy as np
+
+
+def quantize(weights):
+    abs_weights = np.abs(weights)
+    vmax = np.max(abs_weights)
+    s = vmax / 127.
+    qweights = weights / s
+    qweights = np.round(qweights)
+    qweights = qweights.astype(np.int8)
+    return qweights, s
+
+
+def get_weights_biases(weights, weight_name, bias_name='bbb', quant=True):
+    w = weights[weight_name]
+    if quant:
+        w, s = quantize(w)
+        w = tf.constant(w, dtype=tf.float32)
+    else:
+        w = tf.constant(weights[weight_name], dtype=tf.float32)
+        s = 0.
+    try:
+        b = tf.constant(weights[bias_name], dtype=tf.float32)
+    except:
+        b = None
+    return w, b, s
+
 
 def VGG16(x, weights):
     x = tf.reshape(x, shape=[-1, 224, 224, 3])
 
     # block 1
-    w = tf.constant(weights['block1_conv1_W_1:0'], dtype=tf.float32)
-    b = tf.constant(weights['block1_conv1_b_1:0'], dtype=tf.float32)
-    x1 = conv_2d(x, w, b, activation='relu')
+    w, b, s = get_weights_biases(weights, 'block1_conv1_W_1:0', 'block1_conv1_b_1:0')
+    x = conv_2d(x, w, b, s, activation='relu')
 
-    w = tf.constant(weights['block1_conv2_W_1:0'], dtype=tf.float32)
-    b = tf.constant(weights['block1_conv2_b_1:0'], dtype=tf.float32)
-    x = conv_2d(x1, w, b, activation='relu')
+    w, b, s = get_weights_biases(weights, 'block1_conv2_W_1:0', 'block1_conv2_b_1:0')
+    x = conv_2d(x, w, b, s, activation='relu')
     x = maxpool_2d(x, k=2, s=2)
 
     # block2
-    w = tf.constant(weights['block2_conv1_W_1:0'], dtype=tf.float32)
-    b = tf.constant(weights['block2_conv1_b_1:0'], dtype=tf.float32)
-    x = conv_2d(x, w, b, activation='relu')
+    w, b, s = get_weights_biases(weights, 'block2_conv1_W_1:0', 'block2_conv1_b_1:0')
+    x = conv_2d(x, w, b, s, activation='relu')
 
-    w = tf.constant(weights['block2_conv2_W_1:0'], dtype=tf.float32)
-    b = tf.constant(weights['block2_conv2_b_1:0'], dtype=tf.float32)
-    x = conv_2d(x, w, b, activation='relu')
+    w, b, s = get_weights_biases(weights, 'block2_conv2_W_1:0', 'block2_conv2_b_1:0')
+    x = conv_2d(x, w, b, s, activation='relu')
     x = maxpool_2d(x, k=2, s=2)
 
     # block3
-    w = tf.constant(weights['block3_conv1_W_1:0'], dtype=tf.float32)
-    b = tf.constant(weights['block3_conv1_b_1:0'], dtype=tf.float32)
-    x = conv_2d(x, w, b, activation='relu')
+    w, b, s = get_weights_biases(weights, 'block3_conv1_W_1:0', 'block3_conv1_b_1:0')
+    x = conv_2d(x, w, b, s, activation='relu')
 
-    w = tf.constant(weights['block3_conv2_W_1:0'], dtype=tf.float32)
-    b = tf.constant(weights['block3_conv2_b_1:0'], dtype=tf.float32)
-    x = conv_2d(x, w, b, activation='relu')
+    w, b, s = get_weights_biases(weights, 'block3_conv2_W_1:0', 'block3_conv2_b_1:0')
+    x = conv_2d(x, w, b, s, activation='relu')
 
-    w = tf.constant(weights['block3_conv3_W_1:0'], dtype=tf.float32)
-    b = tf.constant(weights['block3_conv3_b_1:0'], dtype=tf.float32)
-    x = conv_2d(x, w, b, activation='relu')
+    w, b, s = get_weights_biases(weights, 'block3_conv3_W_1:0', 'block3_conv3_b_1:0')
+    x = conv_2d(x, w, b, s, activation='relu')
     x = maxpool_2d(x, k=2, s=2)
 
     # block4
-    w = tf.constant(weights['block4_conv1_W_1:0'], dtype=tf.float32)
-    b = tf.constant(weights['block4_conv1_b_1:0'], dtype=tf.float32)
-    x = conv_2d(x, w, b, activation='relu')
+    w, b, s = get_weights_biases(weights, 'block4_conv1_W_1:0', 'block4_conv1_b_1:0')
+    x = conv_2d(x, w, b, s, activation='relu')
 
-    w = tf.constant(weights['block4_conv2_W_1:0'], dtype=tf.float32)
-    b = tf.constant(weights['block4_conv2_b_1:0'], dtype=tf.float32)
-    x = conv_2d(x, w, b, activation='relu')
+    w, b, s = get_weights_biases(weights, 'block4_conv2_W_1:0', 'block4_conv2_b_1:0')
+    x = conv_2d(x, w, b, s, activation='relu')
 
-    w = tf.constant(weights['block4_conv3_W_1:0'], dtype=tf.float32)
-    b = tf.constant(weights['block4_conv3_b_1:0'], dtype=tf.float32)
-    x = conv_2d(x, w, b, activation='relu')
+    w, b, s = get_weights_biases(weights, 'block4_conv3_W_1:0', 'block4_conv3_b_1:0')
+    x = conv_2d(x, w, b, s, activation='relu')
     x = maxpool_2d(x, k=2, s=2)
 
     # block5
-    w = tf.constant(weights['block5_conv1_W_1:0'], dtype=tf.float32)
-    b = tf.constant(weights['block5_conv1_b_1:0'], dtype=tf.float32)
-    x = conv_2d(x, w, b, activation='relu')
+    w, b, s = get_weights_biases(weights, 'block5_conv1_W_1:0', 'block5_conv1_b_1:0')
+    x = conv_2d(x, w, b, s, activation='relu')
 
-    w = tf.constant(weights['block5_conv2_W_1:0'], dtype=tf.float32)
-    b = tf.constant(weights['block5_conv2_b_1:0'], dtype=tf.float32)
-    x = conv_2d(x, w, b, activation='relu')
+    w, b, s = get_weights_biases(weights, 'block5_conv2_W_1:0', 'block5_conv2_b_1:0')
+    x = conv_2d(x, w, b, s, activation='relu')
 
-    w = tf.constant(weights['block5_conv3_W_1:0'], dtype=tf.float32)
-    b = tf.constant(weights['block5_conv3_b_1:0'], dtype=tf.float32)
-    x = conv_2d(x, w, b, activation='relu')
+    w, b, s = get_weights_biases(weights, 'block5_conv3_W_1:0', 'block5_conv3_b_1:0')
+    x = conv_2d(x, w, b, s, activation='relu')
     x = maxpool_2d(x, k=2, s=2)
 
     # fc1
-    w = tf.constant(weights['fc1_W_1:0'], dtype=tf.float32)
-    b = tf.constant(weights['fc1_b_1:0'], dtype=tf.float32)
+    w, b, s = get_weights_biases(weights, 'fc1_W_1:0', 'fc1_b_1:0')
     x = tf.reshape(x, [-1, w.get_shape().as_list()[0]])
-    x = denselayer(x, w, b, activation='relu')
+    x = denselayer(x, w, b, s, activation='relu')
 
     # fc2
-    w = tf.constant(weights['fc2_W_1:0'], dtype=tf.float32)
-    b = tf.constant(weights['fc2_b_1:0'], dtype=tf.float32)
-    x = denselayer(x, w, b, activation='relu')
+    w, b, s = get_weights_biases(weights, 'fc2_W_1:0', 'fc2_b_1:0')
+    x = denselayer(x, w, b, s, activation='relu')
 
     # fc3
-    w = tf.constant(weights['predictions_W_1:0'], dtype=tf.float32)
-    b = tf.constant(weights['predictions_b_1:0'], dtype=tf.float32)
-    x = denselayer(x, w, b)
+    w, b, s = get_weights_biases(weights, 'predictions_W_1:0', 'predictions_b_1:0')
+    x = denselayer(x, w, b, s)
     return x
